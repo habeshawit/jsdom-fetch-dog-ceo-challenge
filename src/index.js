4322337e39f9c01241c0b003cbd4340e35a6bb5d
habeshawit <shalomseleshi@gmail.com>
2021-03-05 23:56:40 -0500

    commit tree 2e4f8b18862a6fe672ce7a446bed8488e3ba8c39

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('%c HI', 'color: firebrick')
+console.log("%c HI", "color: firebrick");
+
+document.addEventListener("DOMContentLoaded", function () {
+  loadImages();
+});
+
+function loadImages() {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+  fetch(imgUrl)
+    .then((res) => res.json())
+    .then((results) => {
+      results.message.forEach((image) => addImage(image));
+    });
+}
+
+function addImage(dogPicUrl) {
+  let container = document.querySelector("#dog-image-container");
+  let newImageDog = document.createElement("img");
+  newImageDog.src = dogPicUrl;
+  container.appendChild(newImageDog);
+}
